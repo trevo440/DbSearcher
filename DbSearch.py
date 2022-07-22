2dcab82923c8494bde426cd10b2d259c31f31061
trevo440 <71515538+trevo440@users.noreply.github.com>
2022-07-22 11:04:58 -0500

    Update DbSearch.py

--- DbSearch.py
+++ DbSearch.py
@@ -465,4 +465,9 @@ class DbSearcher():
         tup = struct.unpack("<6hI2h", dto_value)  # e.g., (2017, 3, 16, 10, 35, 18, 0, -6, 0)
         tweaked = [tup[i] // 100 if i == 6 else tup[i] for i in range(len(tup))]
         return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:07d} {:+03d}:{:02d}".format(*tweaked)
-    
+ '''
+ ~Example Use~
+ dbs = DbSearcher(conn_string="DSN=ExampleDSN", conn_type="OTH", db_name="SampleDatabase")
+ dbs.configure("search_type", "Table")
+ df = dbs.search()
+ '''
